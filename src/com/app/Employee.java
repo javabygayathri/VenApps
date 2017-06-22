735b78f80f9335082ef09056cec50080554f743d
user <user@user-PC>
2017-06-22 13:35:16 +0530

    enter

---
+++ src/com/app/Employee.java
@@ -0,0 +1,20 @@
+package com.app;
+
+public class Employee {
+	
+	private int empId;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String toString() {
+		return "Employee [empId=" + empId + "]";
+	}
+	
+
+}
